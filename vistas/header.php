2eaa63e75973c111009b29bc1eaa250197984b23
Zeus <147615414+zmartinl@users.noreply.github.com>
2024-11-11 12:42:00 +0100

    Add files via upload

---
+++ vistas/header.php
@@ -0,0 +1,4 @@
+<header>
+        <p><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-phone"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" /></svg>+34 924 35 17 61</p>
+        <p><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-mail"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" /><path d="M3 7l9 6l9 -6" /></svg>guadalupe@fundacionloyola.es</p>
+</header>
---
+++ vistas/nav.php
@@ -0,0 +1,5 @@
+<nav>
+    <a href="inico.html">Inico</a>
+    <a href="reservaLibros1User.html">Realizar reserva</a>
+    <a href="verReservasUser.html">Ver reservas</a>
+</nav>
